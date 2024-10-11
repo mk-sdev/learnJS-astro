636b5db4f461f93d9cd20035e208b9287f203ea9
Mikołaj <kaleta222@gmail.com>
2024-10-11 23:32:31 +0200

    feat(index): show chapters and lessons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,21 @@
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 
+export const items = [
+  {
+    label: 'Fundamentals',
+    items: [
+      // Each item here is one entry in the navigation menu.
+      { label: 'comments', slug: 'fundamentals/comments', url: 'dwdw' },
+      { label: 'variables', slug: 'fundamentals/variables', url: 'dwdw' },
+    ],
+  },
+  // {
+  //   label: 'Reference',
+  //   autogenerate: { directory: 'reference' },
+  // },
+]
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -10,20 +25,7 @@ export default defineConfig({
       social: {
         github: 'https://github.com/withastro/starlight',
       },
-      sidebar: [
-        {
-          label: 'Fundamentals',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'comments', slug: 'fundamentals/comments' },
-            { label: 'variables', slug: 'fundamentals/variables' },
-          ],
-        },
-        // {
-        //   label: 'Reference',
-        //   autogenerate: { directory: 'reference' },
-        // },
-      ],
+      sidebar: items,
     }),
   ],
 })
