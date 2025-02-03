efe28a53221006a4cbbc005fbac79630e133faa4
Mikołaj <kaleta222@gmail.com>
2025-02-03 14:41:24 +0100

    data: add Comments and Conditoinals articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,49 @@
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 
-import tunnel from 'astro-tunnel';
+import tunnel from 'astro-tunnel'
 
 export const items = [
   {
     label: 'Fundamentals',
     items: [
-      // Each item here is one entry in the navigation menu.
+      { label: 'first steps', slug: 'fundamentals/first-steps', img: 'dwdw' },
       { label: 'comments', slug: 'fundamentals/comments', img: 'dwdw' },
+      { label: 'operators', slug: 'fundamentals/operators', img: 'dwdw' },
       { label: 'variables', slug: 'fundamentals/variables', img: 'dwdw' },
-      { label: 'variables', slug: 'fundamentals/variables', img: 'dwdw' },
-      { label: 'variables', slug: 'fundamentals/variables', img: 'dwdw' },
-      { label: 'variables', slug: 'fundamentals/variables', img: 'dwdw' },
-      { label: 'variables', slug: 'fundamentals/variables', img: 'dwdw' },
+      {
+        label: 'declaration keywords',
+        slug: 'fundamentals/declaration-keywords',
+        img: 'dwdw',
+      },
+      { label: 'conditionals', slug: 'fundamentals/conditionals', img: 'dwdw' },
+      { label: 'loops', slug: 'fundamentals/loops', img: 'dwdw' },
+      { label: 'functions 1', slug: 'fundamentals/functions-1', img: 'dwdw' },
+      { label: 'functions 2', slug: 'fundamentals/functions-2', img: 'dwdw' },
     ],
   },
   {
     label: 'Data types',
     items: [
-      // Each item here is one entry in the navigation menu.
-      { label: 'strings', slug: 'data-types/strings', img: 'dwdw' },
-      // { label: 'arrays', slug: 'data-types/arrays', img: 'dwdw' },
+      { label: 'introduction', slug: 'data-types/introduction', img: 'dwdw' },
+      { label: 'numbers', slug: 'data-types/numbers', img: 'dwdw' },
+      {
+        label: 'objects fundamentals',
+        slug: 'data-types/objects-fundamentals',
+        img: 'dwdw',
+      },
+      { label: 'arrays', slug: 'data-types/arrays', img: 'dwdw' },
+      { label: 'other types', slug: 'data-types/other-types', img: 'dwdw' },
+      {
+        label: 'iteration techniques',
+        slug: 'data-types/iteration-techniques',
+        img: 'dwdw',
+      },
+      {
+        label: 'type conversion',
+        slug: 'data-types/type-conversion',
+        img: 'dwdw',
+      },
     ],
   },
   // {
